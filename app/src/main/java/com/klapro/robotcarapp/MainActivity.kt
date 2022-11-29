88dd4cceb94ba80eac478f0d1ded51622103ba9f
Kevin <kevinlarocquepro@gmail.com>
2022-11-28 21:19:03 -0500

    wip video and stuff

--- app/src/main/java/com/klapro/robotcarapp/MainActivity.kt
+++ app/src/main/java/com/klapro/robotcarapp/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.klapro.robotcarapp
 
+import android.content.pm.ActivityInfo
+import android.content.pm.ActivityInfo.*
+import android.net.Uri
+import android.opengl.Visibility
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,50 +13,39 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.alexvas.rtsp.widget.RtspSurfaceView
 import com.klapro.robotcarapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var hasScreen: Boolean = false
+    private lateinit var svVideo: RtspSurfaceView
+    private lateinit var tryConnectVideoButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setRequestedOrientation(SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        svVideo = findViewById(R.id.svVideo);
+        tryConnectVideoButton = findViewById(R.id.tryCameraBtn)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        tryConnectVideoButton.setOnClickListener {
+            tryConnectingToTheCamera()
         }
+//        svVideo.start(requestVideo = true, requestAudio = false)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    fun tryConnectingToTheCamera() {
+        // TODO
+        tryConnectVideoButton.visibility = View.GONE
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
+
 }
